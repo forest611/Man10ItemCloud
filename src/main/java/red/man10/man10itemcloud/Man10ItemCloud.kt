fc4646ebe2a3b7faa9ed70b50dee9986a3fb8a94
forest611 <forest611328@gmail.com>
2020-03-08 13:27:16 +0900

    initial commit

---
+++ src/main/java/red/man10/man10itemcloud/Man10ItemCloud.kt
@@ -0,0 +1,20 @@
+package red.man10.man10itemcloud
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.plugin.java.JavaPlugin
+
+class Man10ItemCloud : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        return false
+    }
+}
